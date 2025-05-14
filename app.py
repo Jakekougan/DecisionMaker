a782c1b57e53ce5343c2ac56f2815c70a5c3f260
Jakekougan <jkougan@iwu.edu>
2025-05-13 19:28:29 -0500

    Finished with basic functionality, now it is time to try and create a nice looking UI.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, g, redirect, url_for, render_template, flash, session
 import deci_maker as dm
+import random as rand
 
 app = Flask(__name__)
 
@@ -13,9 +14,10 @@ def home():
 
 @app.route("/form", methods=["GET"])
 def form():
-    func = request.form.get("func")
+    func = request.args.get("func")
     session["type"] = func
-    return render_template("form.html")
+    print(func)
+    return render_template("form.html", func=func)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -24,3 +26,19 @@ def add():
     d.append(item)
     session["ops"] = d
     return render_template('form.html', d=d)
+
+@app.route("/selRand", methods=["GET"])
+def select_random():
+    r = rand.choice(session['ops'])
+    return render_template("sel.html", dec=r)
+
+
+@app.route("/delRand", methods=["GET"])
+def remove_random():
+    d = session["ops"]
+    r = rand.choice(session['ops'])
+    indx = d.index(r)
+    removed = d.pop(indx)
+    flash(f"{removed} has been removed from the pool")
+    return render_template("form.html", removed=removed, d=d)
+
